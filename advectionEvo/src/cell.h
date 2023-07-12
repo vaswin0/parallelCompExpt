a8601feadada5a18061c55fd065d004ddeb6b793
vaswin0 <vaswin0137@gmail.com>
2023-07-13 02:45:53 +0530

    mod for cuda

--- advectionEvo/src/cell.h
+++ advectionEvo/src/cell.h
@@ -17,13 +17,18 @@ class cell
 
   public:
 
-  cell();
-  ~cell();
-  void set_rho(double  );
-  double get_rho( );
-  void add_flux(double ) ; 
-  void clear_flux() ; 
-  void update_rho() ; 
+/*modified with __host__ __device__ decorators */
+
+
+__host__  cell();
+__host__  ~cell();
+__device__ __host__  void set_rho(double  );
+__device__ __host__  double get_rho( );
+__device__ __host__  void add_flux(double ) ; 
+__device__ __host__  void clear_flux() ; 
+__device__ __host__  void update_rho() ; 
+
+friend std::ostream& operator<< (std::ostream & os, const cell & c);
 
 };
 
