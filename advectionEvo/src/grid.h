a831aeb7a8175c2541005e141832ca3733f83621
vaswin0 <vaswin0137@gmail.com>
2023-07-14 03:15:19 +0530

    cuda

--- advectionEvo/src/grid.h
+++ advectionEvo/src/grid.h
@@ -23,25 +23,30 @@ class grid
     int ny ; 
     int nz ; 
     
-    cell* Cell ; 
+    //cell* Cell ; 
 
   public:
 
-    grid(double xmin_, double xmax_, int nx_, double ymin_, double ymax_, int ny_ , double zmin_, double zmax_, double nz_);
-    ~grid();
+  cell* Cell;
 
-    inline int get_nx()   { return nx; }
-    inline int get_ny()   { return ny; }
-    inline int get_nz()   { return nz; }
+__host__    grid(double xmin_, double xmax_, int nx_, double ymin_, double ymax_, int ny_ , double zmin_, double zmax_, double nz_);
+__host__    ~grid();
 
-    inline double get_dx()   { if(nx <= 1 ){return 0;}else{return (xmax - xmin)/(nx-1);} }
-    inline double get_dy()   { if(ny <= 1 ){return 0;}else{return (ymax - ymin)/(ny-1);} }
-    inline double get_dz()   { if(nz <= 1 ){return 0;}else{return (zmax - zmin)/(nz-1);} }
+__device__ __host__    inline int get_nx()   { return nx; }
+__device__ __host__    inline int get_ny()   { return ny; }
+__device__ __host__    inline int get_nz()   { return nz; }
 
-    cell* get_cell(int ix, int iy, int iz);
-    inline double get_cell_x_cordinate(int ix, int iy, int iz){ return xmin + ix * get_dx() ;  }
-    inline double get_cell_y_cordinate(int ix, int iy, int iz){ return ymin + iy * get_dy() ;  }
-    inline double get_cell_z_cordinate(int ix, int iy, int iz){ return zmin + iz * get_dz() ;  }
+__device__ __host__    inline double get_dx()   { if(nx <= 1 ){return 0;}else{return (xmax - xmin)/(nx-1);} }
+__device__ __host__    inline double get_dy()   { if(ny <= 1 ){return 0;}else{return (ymax - ymin)/(ny-1);} }
+__device__ __host__    inline double get_dz()   { if(nz <= 1 ){return 0;}else{return (zmax - zmin)/(nz-1);} }
+
+__device__ __host__    cell* get_cell(int ix, int iy, int iz);
+__device__ __host__    inline double get_cell_x_cordinate(int ix, int iy, int iz){ return xmin + ix * get_dx() ;  }
+__device__ __host__    inline double get_cell_y_cordinate(int ix, int iy, int iz){ return ymin + iy * get_dy() ;  }
+__device__ __host__    inline double get_cell_z_cordinate(int ix, int iy, int iz){ return zmin + iz * get_dz() ;  }
+
+friend std::ostream& operator<<(std::ostream &os, const grid &gr);
+
 
 };
 
